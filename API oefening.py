dca4b27637918c3a3e80b0332c4ce7228dc4a24e
MorschaVG <morschavongrumbkow@gmail.com>
2025-07-19 12:21:16 +0200

    github oefeningen les 7 en 8

---
+++ API oefening.py
@@ -0,0 +1,17 @@
+import requests
+
+# Juiste datumnotatie (als string), en appid als string (tussen haakjes).
+date = "2025-07-14"
+lat = 52.2222
+lon = 4.5337
+appid = "51dcab088e0a7ddf16f7c1aa9fc041e5"
+
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={appid}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Er is iets mis gegaan. Status code: {response.status_code}")
+
---
+++ Github oefeningen/wachtwoord generator.py
@@ -0,0 +1,43 @@
+# Python Wachtwoordgenerator
+#
+# Inleiding:
+#
+# Weet je hoe belangrijk een goed wachtwoord is?
+#
+# Nee, nou als een hacker je probeert te hacken door middel van een bruteforce (alle mogelijkheden proberen die er zijn) dan kan dat variëren van direct tot 63 duizend jaar...
+#
+# 8 karakters	10 karakters	12 karakters
+# Alleen kleine letters	direct	direct	een paar weken
+# + 1 hoofdletter	half uurtje	1 maand	5 jaar
+# + 1 nummer	1 uur	6 jaar	2 duizend jaar
+# + 1 symbool	1 dag	50 jaar	63 duizend jaar
+# Daarnaast wordt dit getal iedere 2 jaar gehalveerd omdat er telkens betere computers bestaan.
+#
+# Check hier je eigen wachtwoord: Password Strength Testing Tool
+#
+# Het is daarom goed om een sterk wachtwoord te kiezen die je eigenlijk om de 2 jaar moet aanpassen. Maar elke keer weer een goed wachtwoord bedenken kost jou heel veel denkwerk. Maar gelukkig kan jij als developer er een scriptje voor schrijven.
+#
+# Instructies
+#
+# De applicatie zal je 3 vragen stellen:
+#
+# Hoeveel letters wil je in je wachtwoord?
+# Hoeveel symbolen wil je in je wachtwoord?
+# Hoeveel nummers wil je in je wachtwoord?
+# Het doel van de applicatie is om de invoer van de gebruiker te ontvangen en vervolgens een willekeurig wachtwoord te genereren.
+#
+# Eenvoudige Versie (Stap 1)
+# Genereer het wachtwoord in een vooraf bepaalde volgorde. Bijvoorbeeld, als de gebruiker een wachtwoord wil met:
+#
+# 4 letters
+# 2 symbolen
+# 3 nummers
+# dan zou het wachtwoord er als volgt uit kunnen zien:
+#
+# qfIw)+393
+#
+# Je ziet dat alle letters bij elkaar staan, alle symbolen bij elkaar, en alle nummers volgen elkaar ook op.
+#
+# Kom je er niet uit welke module je moet gebruiken?
+# Kom je er niet uit hoe je een willekeurig karakter aanspreekt?
+
