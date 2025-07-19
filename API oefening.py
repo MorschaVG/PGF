8d9cb7ddbd31651a4cda2ab868d649992e5f07b5
MorschaVG <morschavongrumbkow@gmail.com>
2025-07-19 12:46:36 +0200

    projectmap naam aangepast en api oefening

--- API oefening.py
+++ API oefening.py
@@ -1,5 +1,15 @@
 import requests
 
+
+def get_weather_data(city_name, api_key):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Er is iets mis gegaan. Status code: {response.status_code}")
+
+
 # Juiste datumnotatie (als string), en appid als string (tussen haakjes).
 date = "2025-07-14"
 lat = 52.2222
@@ -8,10 +18,4 @@ appid = "51dcab088e0a7ddf16f7c1aa9fc041e5"
 
 url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={appid}"
 
-response = requests.get(url)
-
-if response.status_code == 200:
-    print(response.json())
-else:
-    print(f"Er is iets mis gegaan. Status code: {response.status_code}")
-
+r
